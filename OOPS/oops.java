b11aeb04100bee25ed3540347e4146a5183d4be5
Kanhaiya Jee <kanhaiyajee0852@gmail.com>
2026-06-29 19:08:48 +0530

    deep skilling

---
+++ OOPS/oops.java
@@ -0,0 +1,62 @@
+package OOPS;
+
+public class oops {
+    public static void main(String[] args) {
+        pen p1 = new pen();
+        p1.setColor("blue");
+        System.out.println(p1.color);
+        p1.setTip(1);
+        System.out.println(p1.tip);
+
+        student s1 = new student();
+        s1.setName("Alice");
+        s1.setAge(20);
+        s1.setPercentage(85, 90, 78);
+
+
+        BankAccount myAcc = new BankAccount();
+        myAcc.username = "Kanhaiya";
+        myAcc.username = "Kanhaiya123";
+        System.out.println(myAcc.username);
+        //System.out.println(myAcc.password); // This will cause an error because password is private
+    }
+}
+
+class BankAccount{
+    public String username;
+    private String password;
+
+    public void setPassword(String newPassword){
+        password = newPassword;
+    }
+}
+
+class pen{
+    //prop +  function
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+class student{
+    String name;
+    int age;
+    float percentage;
+
+    void setPercentage(int phy, int chem, int math  ){
+        percentage = (phy + chem + math) / 3.0f;
+    }
+
+    void setName(String newName){
+        name = newName;
+    }
+    void setAge(int newAge){
+        age = newAge;
+    }
+}
